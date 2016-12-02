eccc29db03b358f82fc2143fc4698a0884159006
Lui <arai_lui@lob-inc.com>
2016-12-02 07:11:48 +0000

    microposts

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,12 +1,14 @@
 class UsersController < ApplicationController
+  before_action :set_user, only: [:show, :edit, :update]
+  before_action :correct_user, only: [:edit, :update]
   
   def show # 追加
-   @user = User.find(params[:id])
   end
 
   def new
     @user = User.new
   end
+
   def create
     @user = User.new(user_params)
     if @user.save
@@ -17,10 +19,30 @@ class UsersController < ApplicationController
     end
   end
 
+  def edit
+  end
+
+  def update
+    if @user.update(user_params)
+      flash[:success] = "Update Profile!"
+      redirect_to @user
+    else
+      render 'edit'
+    end
+  end
+
   private
 
   def user_params
     params.require(:user).permit(:name, :email, :password,
-                                 :password_confirmation)
+                                 :password_confirmation, :location)
+  end
+
+  def correct_user
+    redirect_to root_path if current_user != @user
+  end
+  
+  def set_user
+    @user = User.find(params[:id])
   end
 end
